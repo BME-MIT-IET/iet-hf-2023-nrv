4d2b5da5de4ce9f760aacaef24ef7565d9ff4d88
strbikanna <strbik.ann2000@gmail.com>
2023-05-27 11:25:30 +0200

    field teszt

--- FulloschQtyusch/src/test/java/model/map/FieldTest.java
+++ FulloschQtyusch/src/test/java/model/map/FieldTest.java
@@ -23,67 +23,67 @@ class FieldTest {
 
   @Test
   void virologistsOnFieldTest() {
-    field.AddVirologist(new Virologist());
-    field.AddVirologist(new Virologist());
+    field.addVirologist(new Virologist());
+    field.addVirologist(new Virologist());
     Virologist mockVirologist = mock(Virologist.class);
-    verify(mockVirologist, times(1)).SetField(field);
-    field.AddVirologist(mockVirologist);
-    Assertions.assertFalse(field.GetVirologists().isEmpty());
-    Assertions.assertEquals(3, field.GetVirologists().size());
-    Assertions.assertTrue(field.GetVirologists().contains(mockVirologist));
+    verify(mockVirologist, times(1)).setField(field);
+    field.addVirologist(mockVirologist);
+    Assertions.assertFalse(field.getVirologists().isEmpty());
+    Assertions.assertEquals(3, field.getVirologists().size());
+    Assertions.assertTrue(field.getVirologists().contains(mockVirologist));
   }
 
   @Test
   void neighboursTest() {
-    Assertions.assertTrue(field.GetNeighbours().isEmpty());
+    Assertions.assertTrue(field.getNeighbours().isEmpty());
     Field n1 = new Field();
     Laboratory n2 = new Laboratory(new ForgetCode());
     InfectedLaboratory n3 = new InfectedLaboratory(new StunCode());
     Field notNeighbour = new Field();
-    field.AddNeighbour(n1);
-    field.AddNeighbour(n2);
-    field.AddNeighbour(n3);
-    Assertions.assertTrue(field.GetNeighbours().contains(n1));
-    Assertions.assertTrue(n1.GetNeighbours().contains(field));
-    Assertions.assertTrue(field.GetNeighbours().contains(n2));
-    Assertions.assertTrue(n2.GetNeighbours().contains(field));
-    Assertions.assertTrue(field.GetNeighbours().contains(n3));
-    Assertions.assertTrue(n3.GetNeighbours().contains(field));
-    Assertions.assertFalse(notNeighbour.GetNeighbours().contains(field));
+    field.addNeighbour(n1);
+    field.addNeighbour(n2);
+    field.addNeighbour(n3);
+    Assertions.assertTrue(field.getNeighbours().contains(n1));
+    Assertions.assertTrue(n1.getNeighbours().contains(field));
+    Assertions.assertTrue(field.getNeighbours().contains(n2));
+    Assertions.assertTrue(n2.getNeighbours().contains(field));
+    Assertions.assertTrue(field.getNeighbours().contains(n3));
+    Assertions.assertTrue(n3.getNeighbours().contains(field));
+    Assertions.assertFalse(notNeighbour.getNeighbours().contains(field));
   }
 
   @Test
   void removeVirologistTest() {
     Virologist mockVirologist = mock(Virologist.class);
-    Assertions.assertFalse(field.GetVirologists().contains(mockVirologist));
-    field.AddVirologist(mockVirologist);
-    verify(mockVirologist, times(1)).SetField(field);
-    Assertions.assertTrue(field.GetVirologists().contains(mockVirologist));
-    field.RemoveVirologist(mockVirologist);
-    Assertions.assertFalse(field.GetVirologists().contains(mockVirologist));
+    Assertions.assertFalse(field.getVirologists().contains(mockVirologist));
+    field.addVirologist(mockVirologist);
+    verify(mockVirologist, times(1)).setField(field);
+    Assertions.assertTrue(field.getVirologists().contains(mockVirologist));
+    field.removeVirologist(mockVirologist);
+    Assertions.assertFalse(field.getVirologists().contains(mockVirologist));
   }
 
   @Test
   void learnGeneticCodeTest() {
     Virologist mockVirologist = mock(Virologist.class);
-    field.LearnGeneticCode(mockVirologist);
-    verify(mockVirologist, times(0)).Learn();
+    field.learnGeneticCode(mockVirologist);
+    verify(mockVirologist, times(0)).learn();
   }
 
   @Test
   void collectMaterialOnFieldTest() {
     Virologist mockVirologist = mock(Virologist.class);
-    field.CollectMaterial(mockVirologist);
-    verify(mockVirologist, times(0)).AddNucleotide(anyInt());
-    verify(mockVirologist, times(0)).AddAminoAcid(anyInt());
+    field.collectMaterial(mockVirologist, Material.GENERIC);
+    verify(mockVirologist, times(0)).addNucleotide(anyInt());
+    verify(mockVirologist, times(0)).addAminoAcid(anyInt());
   }
 
   @Test
   void pickUpEquipmentTest() {
     Virologist mockVirologist = mock(Virologist.class);
     Equipment equipment = mock(Equipment.class);
-    field.Drop(equipment);
-    field.PickUpEquipment(mockVirologist);
-    verify(mockVirologist, times(1)).AddEquipment(equipment);
+    field.drop(equipment);
+    field.pickUpEquipment(mockVirologist);
+    verify(mockVirologist, times(1)).addEquipment(equipment);
   }
 }
